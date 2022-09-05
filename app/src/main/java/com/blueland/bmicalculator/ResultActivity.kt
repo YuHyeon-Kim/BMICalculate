edd8a9fe327b187c9a1689c85db536ad69086cb5
김유현 <dbgus3577@gmail.com>
2022-09-05 21:07:58 +0900

    setContentView 제거

--- app/src/main/java/com/blueland/bmicalculator/ResultActivity.kt
+++ app/src/main/java/com/blueland/bmicalculator/ResultActivity.kt
@@ -12,7 +12,6 @@ class ResultActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_result)
 
         val height = intent.getIntExtra("height", 0)
         val weight = intent.getIntExtra("weight", 0)
